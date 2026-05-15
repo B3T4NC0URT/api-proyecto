1e8a080871769a455808acdf80df79d4fe8e47aa
B3T4NC0URT <134117174+B3T4NC0URT@users.noreply.github.com>
2026-05-14 23:15:08 -0500

    se agregaron los datos

---
+++ datos/clientes.js
@@ -0,0 +1,23 @@
+let clientes ={
+  santiago : {
+    id = 1,
+    telefono:530930505
+  },
+  arturo:{
+    id:2,
+    telefono:530930505,
+
+  },
+  pedro:{
+    id:3,
+    telefono:539230505
+  },
+  juan:{
+    id:4,
+    telefono:53523095
+  },
+  pepito:{
+    id:5,
+    telefono:123490479
+  }
+}
---
+++ datos/ordenes.js
@@ -0,0 +1,48 @@
+let ordenes ={
+  pendientes:[
+    {
+      id:1,
+      nombre:"santiago",
+      telefono:123456789,
+      marcaTelefono:"samsung",
+      modelo:"a32",
+      descripcion:"pantalla mala"
+    },
+    {
+      id:2,
+      nombre:"pedro",
+      telefono:539230505,
+      marcaTelefono:"redmi",
+      modelo:"note 14",
+      descripcion:"no carga"
+    },    
+    {
+      id:3,
+      nombre:"juan",
+      telefono:53523095,
+      marcaTelefono:"tecno",
+      modelo:"pov 20",
+      descripcion:"se mojo"
+    }
+  ],
+  completada:[
+        {
+      id:1,
+      nombre:"pepito",
+      telefono:123490479,
+      marcaTelefono:"samsung",
+      modelo:"s22",
+      descripcion:"cambio bateria"
+    },
+    {
+      id:2,
+      nombre:"arturo",
+      telefono:530930505,
+      marcaTelefono:"redmi",
+      modelo:"note 15 pro",
+      descripcion:"recuperacion de softwar"
+    }
+  ]
+
+};
+module.exports.ordenes = ordenes;
