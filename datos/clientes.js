6efe2c13f3f7a2bff23a199e5392faa3efafa788
B3T4NC0URT <134117174+B3T4NC0URT@users.noreply.github.com>
2026-05-21 19:39:58 -0500

    refactorizacion ruta de ordenes y agregar controlador

--- datos/clientes.js
+++ datos/clientes.js
@@ -1,26 +1,31 @@
 let clientes = [
   {
     id:1,
+    documento:1234567,
     telefono:530930505,
     nombre:"santiago"
   },
   {
     id:2,
+    cedula:1324567,
     telefono:530930505,
     nombre:"arturo"
   },
   {
     id:3,
+    cedula:1423567,
     telefono:539230505,
     nombre:"pedro"
   },
   {
     id:4,
+    cedula:1243567,
     telefono:53523095,
     nombre: "juan"
   },
   {
     id:5,
+    cedula:1523467,
     telefono:123490479,
     nombre:"pepito"
   }
--- datos/ordenes.js
+++ datos/ordenes.js
@@ -1,12 +1,13 @@
-let ordenes ={
-  pendientes:[
-    {
+let ordenes = {
+    ordenes :[
+      {
       id:1,
       nombre:"santiago",
       telefono:123456789,
       marcaTelefono:"samsung",
       modelo:"a32",
-      descripcion:"pantalla mala"
+      descripcion:"pantalla mala",
+      estado:"pendiente"
     },
     {
       id:2,
@@ -14,7 +15,8 @@ let ordenes ={
       telefono:539230505,
       marcaTelefono:"redmi",
       modelo:"note 14",
-      descripcion:"no carga"
+      descripcion:"no carga",
+      estado:"pendiente"
     },    
     {
       id:3,
@@ -22,27 +24,27 @@ let ordenes ={
       telefono:53523095,
       marcaTelefono:"tecno",
       modelo:"pov 20",
-      descripcion:"se mojo"
-    }
-  ],
-  completadas:[
-        {
-      id:1,
+      descripcion:"se mojo",
+      estado:"cancelada"
+    },
+    {
+      id:4,
       nombre:"pepito",
       telefono:123490479,
       marcaTelefono:"samsung",
       modelo:"s22",
-      descripcion:"cambio bateria"
+      descripcion:"cambio bateria",
+      estado:"procesando"
     },
     {
-      id:2,
+      id:5,
       nombre:"arturo",
       telefono:530930505,
       marcaTelefono:"redmi",
       modelo:"note 15 pro",
-      descripcion:"recuperacion de software"
+      descripcion:"recuperacion de software",
+      estado:"completada"
     }
   ]
-
 };
-module.exports.ordenes = ordenes;
+module.exports = ordenes;
